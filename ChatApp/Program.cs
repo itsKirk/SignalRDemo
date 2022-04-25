fbc2d375a0f0b1b32569d6144257da7e1da3d585
Kirk <kirkrepository@gmail.com>
2022-04-25 20:09:44 +0300

    Hub Configuration

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -1,12 +1,15 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using ChatApp.Data;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddResponseCompression(options =>
+{
+  options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +27,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+
+app.MapHub<ApplicationHub>("apphub");
+
 app.MapFallbackToPage("/_Host");
 
 app.Run();
