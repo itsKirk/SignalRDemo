21b142cd0405921445d4a2f114f734241b6efd8f
Kirk <kirkrepository@gmail.com>
2022-04-27 20:13:13 +0300

    screen-text configuration

---
+++ ChatApp/Data/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Data
+{
+  public class ChatMessage
+  {
+    public string? Message { get; set; }
+    public string? Sender { get; set; }
+    public bool IsActiveSender { get; set; }
+    public string TimeSent { get; set; } = DateTime.Now.ToLocalTime().ToShortTimeString();
+  }
+}
